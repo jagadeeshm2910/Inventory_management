16a75f9a607cf217da1bdf0ef92a56d61c397f65
Jagadeesh <Jagadeesh_M@comcast.com>
2025-07-21 22:31:26 +0530

    render

--- Inventory_Backend/fastapi-template/app/database.py
+++ Inventory_Backend/fastapi-template/app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL") or os.getenv("DATABASE_URL")
-SQLALCHEMY_DATABASE_URL = "SQLALCHEMY_DATABASE_URL"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
