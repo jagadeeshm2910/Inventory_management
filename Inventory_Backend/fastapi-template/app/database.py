d7a1497e18cc645d7e069f138d982c38ce9d3ac4
Jagadeesh <Jagadeesh_M@comcast.com>
2025-07-21 18:49:28 +0530

    render

--- Inventory_Backend/fastapi-template/app/database.py
+++ Inventory_Backend/fastapi-template/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/mydb"
+SQLALCHEMY_DATABASE_URL = "DATABASE_URL"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
